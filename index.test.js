d3a3161bf3b4be12048c70eafcff64212d9d9b92
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-24 16:52:51 -0700

    wrote a passing test for my mapping function

--- index.test.js
+++ index.test.js
@@ -2,6 +2,10 @@ const { mapFunction } = require('./index.js');
 
 describe('creating my own array method functions', () => {
   it('map function: returns the mapped new array', () => {
-    
+    const array = [1, 2, 3, 4];
+    const mapping = mapFunction(array, callback => {
+      return callback * 5;
+    });
+    expect(mapping).toEqual([5, 10, 15, 20]);
   });
 });
