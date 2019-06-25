35c21f1e415a78265745072a2a14efdcf511bf89
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-25 11:12:52 -0700

    refactored my callbacks in my tests, realized I wasn't quite clear on how I was using the callbacks in the testing suit, renaming them was still a little confusing, but Kate suggested I put the first in () even though it is not necessary, and that is helping me to practice while remembering that the first (item) is the argument that I am passing an anonymous callback function, I had thought callback was the name of the function

--- index.test.js
+++ index.test.js
@@ -3,16 +3,16 @@ const { mapFunction, filterFunction, findIndexFunction, everyFunction } = requir
 describe('mapped function', () => {
   it('returns the mapped new array with the values * 5', () => {
     const array = [1, 2, 3, 4];
-    const mapping = mapFunction(array, callback => {
-      return callback * 5;
+    const mapping = mapFunction(array, (item) => {
+      return item * 5;
     });
     expect(mapping).toEqual([5, 10, 15, 20]);
   });
 
   it('returns the mapped new array with all uppercase strings', () => {
     const array = ['testing', 'this', 'again'];
-    const mapping = mapFunction(array, callback => {
-      return callback.toUpperCase();
+    const mapping = mapFunction(array, (item) => {
+      return item.toUpperCase();
     });
     expect(mapping).toEqual(['TESTING', 'THIS', 'AGAIN']);
   });
@@ -21,24 +21,24 @@ describe('mapped function', () => {
 describe('filter function', () => {
   it('returns the filtered new array with numbers greater than 2', () => {
     const array = [1, 2, 3, 4];
-    const filtering = filterFunction(array, callback => {
-      return callback > 2;
+    const filtering = filterFunction(array, (item) => {
+      return item > 2;
     });
     expect(filtering).toEqual([3, 4]);
   });
 
   it('returns the filtered new array with only even numbers', () => {
     const array = [1, 2, 3, 4];
-    const filtering = filterFunction(array, callback => {
-      return callback % 2 === 0;
+    const filtering = filterFunction(array, (item) => {
+      return item % 2 === 0;
     });
     expect(filtering).toEqual([2, 4]);
   });
 
   it('returns the filtered new array of strings with letter i', () => {
     const array = ['hi', 'world', 'stuff', 'pix'];
-    const filtering = filterFunction(array, callback => {
-      return callback.includes('i');
+    const filtering = filterFunction(array, (item) => {
+      return item.includes('i');
     });
     expect(filtering).toEqual(['hi', 'pix']);
   });
@@ -47,24 +47,24 @@ describe('filter function', () => {
 describe('indexOf function', () => {
   it('the function returns an index of the array that includes letter d', () => {
     const array = ['dude', 'hey', 'dog'];
-    const indexOf = findIndexFunction(array, callback => {
-      return callback.includes('d');
+    const indexOf = findIndexFunction(array, (item) => {
+      return item.includes('d');
     });
     expect(indexOf).toEqual(0);
   });
 
   it('the function returns the index of first item in array', () => {
     const array = [1, 2, 3];
-    const indexOf = findIndexFunction(array, callback => {
-      return callback % 2 === 0;
+    const indexOf = findIndexFunction(array, (item) => {
+      return item % 2 === 0;
     });
     expect(indexOf).toEqual(1);
   });
 
   it('the function returns -1', () => {
     const array = [1, 7, 3];
-    const indexOf = findIndexFunction(array, callback => {
-      return callback % 2 === 0;
+    const indexOf = findIndexFunction(array, (item) => {
+      return item % 2 === 0;
     });
     expect(indexOf).toEqual(-1);
   });
@@ -73,16 +73,16 @@ describe('indexOf function', () => {
 describe('every function', () => {
   it('the function returns true', () => {
     const array = [4, 2, 8];
-    const every = everyFunction(array, callback => {
-      return callback % 2 === 0;
+    const every = everyFunction(array, (item) => {
+      return item % 2 === 0;
     });
     expect(every).toBe(true);
   });
 
   it('the function returns false', () => {
     const array = [7, 5, 9];
-    const every = everyFunction(array, callback => {
-      return callback % 2 === 0;
+    const every = everyFunction(array, (item) => {
+      return item % 2 === 0;
     });
     expect(every).toBe(false);
   });
